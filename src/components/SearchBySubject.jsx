0167cd2eeb6d0fe6ab0faaabea77e365c361fd36
Olivia Kim <chaeyoon1116@gmail.com>
2025-03-01 19:21:49 -0800

    searchBySubject is working

--- src/components/SearchBySubject.jsx
+++ src/components/SearchBySubject.jsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
+
 
 export function SearchBySubject(props) {
+    const {selected, userKeyword, handleSelect, handleInput, applyFilter} = props;
 
     const optionsArray = props.titleNames.map( (title) => {
         const transformed = (
-            <option value={title}>{title}</option>
+            <option key={title}>{title}</option>
         )
         return transformed;
     })
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        applyFilter(userKeyword, selected);
+    }
     
+
     
     
     return (
         <div className="container">
             <form className="searchBar">
                 <label for="search keyword">Search: </label>
-                <input className="form-control me-2" type="search" placeholder="Type keyword.." />
-                <select>
+                <input className="form-control me-2" type="search" placeholder="Type keyword.." value={userKeyword} onChange={handleInput}/>
+                <select onChange={handleSelect}>
+                    <option key="none">none</option>
                     {optionsArray}
                 </select>
-                <button className="btn buttonStyle" type="submit">Search</button>
+                <button className="btn buttonStyle" type="submit" onClick={handleClick}>Search</button>
             </form>
         </div>
     )
