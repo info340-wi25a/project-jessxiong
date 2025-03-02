19e345b62aab9935463d2ce4328cb46482db551d
jesxiong <jesxiong@uw.edu>
2025-03-02 15:54:42 -0800

    fixing hamburger menu and adding help

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,47 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
-  return (
-    <header>
-      <nav className="navbar navbar-expand-md navbar-light">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            <img
-              src="/img/6585301_books_education_learning_school_study_icon.png"
-              width="20"
-              height="20"
-              alt="Home"
-            />
-            FocusFlow
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/timer">Timer</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/subject">Notes</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/help">Help</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <header>
+            <nav className="navbar navbar-expand-md navbar-light">
+                <div className="container">
+                <Link className="navbar-brand" to="/">
+                    <img src="/img/6585301_books_education_learning_school_study_icon.png" width="20" height="20" alt="Home"/>
+                    FocusFlow
+                </Link>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    onClick={() => setIsOpen(!isOpen)}
+                    aria-expanded={isOpen}
+                    aria-label="Toggle nav"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`} id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/timer">Timer</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/subject">Notes</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/help">Help</Link>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Navbar;
