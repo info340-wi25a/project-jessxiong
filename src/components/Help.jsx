b87cbebfb9fbdd4c313ce8e9b7d84a1011adcb0f
eunjiii <eunjiii@uw.edu>
2025-03-16 15:39:18 -0700

    more instructions on help

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -39,7 +39,7 @@ export function Help() {
             <h2>Notes</h2>
             <ul>
                 <li>
-                    You can navigate to the Notes Page through clicking on the "Notes" text on the navigation bar that is located at the top of the page
+                    You can navigate to the Notes Page through clicking on the "Notes" text on the navigation bar that is located at the top of the page.
                 </li>
                 <li>
                     After clicking "Notes", you will be taken to "Subject Notes Page" where you can add and delete subject folders and also access previous notes. To access your notes, click on the image of the subject card. You can also sort and search by each subject.
@@ -48,7 +48,10 @@ export function Help() {
                     After clicking a subject card, you will be taken to "Notes for (Subject)" page where you can add, delete, and edit notes within the subject folder. You can search through your notes through date.
                 </li>
                 <li>
-                    If you click on existing files in the subject folder, you will be able to edit notes. And once you save the edits on the notes, you will be taken back to the subject folder and see the lists of notes again.
+                    If you click on existing notes in the subject folder, you will be able to edit notes. You can also use timer while taking notes, it isn't necessarily synced with the timer on timer page.
+                </li>
+                <li>
+                    Once you save the edits on the notes, you will be taken back to the subject folder and see the lists of notes again.
                 </li>
             </ul>
 
